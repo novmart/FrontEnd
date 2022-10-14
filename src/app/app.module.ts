90ba57d91d84b0a0b8c00d64df8aa1682091335f
novmart <novellocncm1@gmail.com>
2022-10-14 18:43:13 +0100

    Se agrego el header, el banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { LogoAPComponent } from './component/logo-ap/logo-ap.component';
+import { SocialComponent } from './component/social/social.component';
+import { BannerComponent } from './component/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
